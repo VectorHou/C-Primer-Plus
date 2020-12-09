fff8e14624c1b4431f97d699730303a9f1218057
Hou Zhiguo (uidq1723) <zhiguo.hou@desay-svautomotive.com>
2020-12-09 17:10:00 +0800

    add CHP2

---
+++ CHP2/program practice/main.cpp
@@ -0,0 +1,43 @@
+#include "header"
+
+
+void DisplayNameAndAddress(string name, string address)
+{
+
+}
+
+void ConvertInputFurlongToYard()
+{
+    
+}
+
+void Using3Function()
+{
+    
+}
+
+void ConvertYearsToMonths()
+{
+    
+}
+
+void ConvertCelsiusToFahrenheit()
+{
+    
+}
+
+void ConvertLightyearToAstronomicalunits()
+{
+    
+}
+
+void DisplayHoursAndMinutes()
+{
+    
+}
+
+int main()
+{
+    cout<<"Hello, world"<<std::endl;
+    return 0;
+}
